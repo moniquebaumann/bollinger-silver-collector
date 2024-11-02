e4294d6e11014d72c4d961622fdbe02221211ee8
Monique Baumann <monique@freedomcash.org>
2024-11-02 11:20:29 +0100

    enjoy bollingers party

--- bollinger.ts
+++ bollinger.ts
@@ -17,7 +17,7 @@ export class FreedomCashProvider {
         return FreedomCashProvider.instance
     }
 
-    private readonly minHistoryLength = 3
+    private readonly historyLength = 9
     private readonly minCollateralPercentage = 55
     private roundCounter
     private address
@@ -41,7 +41,7 @@ export class FreedomCashProvider {
             } catch (error: any) {
                 console.log(error.message);
             }
-        }, 18 * 1000)
+        }, 9 * 1000)
     }
 
     private async playRound() {
@@ -53,15 +53,19 @@ export class FreedomCashProvider {
         response = await this.indexerClient.account.getSubaccountPerpetualPositions(this.address, 0);
         const positions = response.positions;
         for (const position of positions) {
+
+            if (position.market !== 'BTC-USD') { continue }
             if (position.closedAt === null) {
-                const pnlInPerCent = (position.unrealizedPnl * 100) / Math.abs(position.size)
+                const pnlInPerCent = (position.unrealizedPnl * 100) / (Math.abs(position.size) * position.entryPrice)
                 this.updatePNLHistory(position.market, pnlInPerCent)
                 const pnlHistory = this.pnlHistories.filter((e: IPNLHistory) => e.market === position.market)[0]
-                if (pnlHistory.pnls.length > this.minHistoryLength) {
-                    const bollingerBands = Bands.getBollingerBands(pnlHistory.pnls, 2)
-                    const lower = bollingerBands.lower[this.roundCounter - 1]
-                    const current = pnlHistory.pnls[this.roundCounter - 1]
-                    const upper = bollingerBands.upper[this.roundCounter - 1]
+                // console.log(`pnlHistory: ${pnlHistory.pnls}`)
+                // console.log(`pnlHistory: ${JSON.stringify(pnlHistory)}`)
+                if (pnlHistory.pnls.length === this.historyLength) {
+                    const bollingerBands = Bands.getBollingerBands(pnlHistory.pnls, 5)
+                    const lower = bollingerBands.lower[pnlHistory.pnls.length - 1]
+                    const current = pnlHistory.pnls[pnlHistory.pnls.length - 1]
+                    const upper = bollingerBands.upper[pnlHistory.pnls.length - 1]
                     const advice = this.getAdvice(lower, current, upper, freeCollateralPercentage)
                     console.log(advice)
                     const wallet = await LocalWallet.fromMnemonic(this.mnemonic, BECH32_PREFIX);
@@ -72,14 +76,14 @@ export class FreedomCashProvider {
         }
     }
 
-    private getAdvice(lower: number, current: number, upper: number, freeCollateralPercentage: number){
-        console.log(lower)
-        console.log(current)
-        console.log(upper)
-        if(current < lower && freeCollateralPercentage > this.minCollateralPercentage) {
-            return "IncreaseExposure"
-        } else if( current > upper || freeCollateralPercentage < this.minCollateralPercentage) {
-            return "DecreaseExposure"
+    private getAdvice(lower: number, current: number, upper: number, freeCollateralPercentage: number) {
+        // console.log(`current: ${current}`)
+        // console.log(`would buy at: ${lower}`)
+        // console.log(`would sell at: ${upper}`)
+        if (current <= lower && freeCollateralPercentage > this.minCollateralPercentage) {
+            return "Buy"
+        } else if (current >= upper || freeCollateralPercentage < this.minCollateralPercentage) {
+            return "Sell"
         } else {
             return "Relax"
         }
@@ -89,6 +93,9 @@ export class FreedomCashProvider {
         const pnlHistory = this.pnlHistories.filter((e: IPNLHistory) => e.market === market)[0]
         if (pnlHistory === undefined) {
             this.pnlHistories.push({ market: market, pnls: [currentPNL] })
+        } else if (pnlHistory.pnls.length === this.historyLength) {
+            pnlHistory.pnls.splice(0, 1)
+            pnlHistory.pnls.push(currentPNL)
         } else {
             pnlHistory.pnls.push(currentPNL)
         }
@@ -100,14 +107,14 @@ export class FreedomCashProvider {
         const id = `${this.roundCounter}-${position.market}`
         let goodTilTimeInSeconds1 = OrderTimeInForce.IOC
         let side, price
-        if (advice === "DecreaseExposure" && Math.abs(position.size) > marketData.stepSize) {
+        if (advice === "Sell" && Math.abs(position.size) > marketData.stepSize) {
             console.log(`taking profits with ${position.market}`)
-            side = (position.side === "SHORT") ? OrderSide.BUY : OrderSide.SELL
+            side = OrderSide.SELL
             price = (side === OrderSide.BUY) ? marketData.oraclePrice * 1.001 : marketData.oraclePrice * 0.999
-        } else if (advice === "IncreaseExposure") {
+        } else if (advice === "Buy") {
             console.log(`increasing the exposure for position ${position.market}`)
-            side = (position.side === "SHORT") ? OrderSide.SELL : OrderSide.BUY
-            price = (side === OrderSide.BUY) ? marketData.oraclePrice * 1.01 : marketData.oraclePrice * 0.99
+            side = OrderSide.BUY
+            price = (side === OrderSide.BUY) ? marketData.oraclePrice * 1.001 : marketData.oraclePrice * 0.999
             goodTilTimeInSeconds1 = OrderTimeInForce.GTT
         } else {
             console.log(`not doing anything with ${position.market} atm`)
