b3b673a4a637af3f74d563c6c3252f7f2afeaadf
Monique Baumann <monique@freedomcash.org>
2024-11-03 00:08:00 +0100

    enjoy the bollinger party

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 historyLength=60
 celebrateAt=1
 intervalLength=24
-targetCollateralPercentage=30
-minCollateralPercentage=20
+targetCollateralPercentage=60
+minCollateralPercentage=30
 stepSizeFactor=9
-spreadFactor=12
+spreadFactor=18
 
 pm2 start ts-node --name=collect -- -P tsconfig.json collector.ts $historyLength $celebrateAt $intervalLength $targetCollateralPercentage $minCollateralPercentage $stepSizeFactor $spreadFactor
