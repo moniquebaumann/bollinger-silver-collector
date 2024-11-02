b84b8191c6d2da0636bced153af66c67f8481482
Monique Baumann <monique@freedomcash.org>
2024-11-02 20:58:46 +0100

    enjoy bollingers party

--- bollinger.ts
+++ bollinger.ts
@@ -17,9 +17,11 @@ export class FreedomCashProvider {
         return FreedomCashProvider.instance
     }
 
-    private readonly historyLength = 33
-    private readonly targetCollateralPercentage = 65
-    private readonly minCollateralPercentage = 35
+    private readonly historyLength =60
+    private readonly celebrateAt = 1
+    private readonly intervalLength = 60
+    private readonly targetCollateralPercentage = 30
+    private readonly minCollateralPercentage = 20
     private roundCounter
     private address
     private mnemonic
@@ -42,43 +44,34 @@ export class FreedomCashProvider {
             } catch (error: any) {
                 console.log(error.message);
             }
-        }, 60 * 1000)
+        }, this.intervalLength * 1000)
     }
 
     private async playRound() {
         this.roundCounter++
         let response = await this.indexerClient.account.getSubaccounts(this.address);
-        const freeCollateralPercentage = (response.subaccounts[0].freeCollateral * 100) / response.subaccounts[0].equity
-        console.log(`equity: ${response.subaccounts[0].equity} \nfree: ${response.subaccounts[0].freeCollateral} at round number ${this.roundCounter}`)
+        const freeCollatPercentage = Number(((response.subaccounts[0].freeCollateral * 100) / response.subaccounts[0].equity).toFixed(2))
+        console.log(`equity: ${response.subaccounts[0].equity} \nfree: ${freeCollatPercentage} % at round number ${this.roundCounter}`)
         response = await this.indexerClient.account.getSubaccountPerpetualPositions(this.address, 0);
         const positions = response.positions;
         for (const position of positions) {
+            // if (position.market !== "DOGE-USD") { continue }
             if (position.closedAt === null) {
                 const pnlInPerCent = (position.unrealizedPnl * 100) / (Math.abs(position.size) * position.entryPrice)
                 this.updatePNLHistory(position.market, pnlInPerCent)
                 const pnlHistory = this.pnlHistories.filter((e: IPNLHistory) => e.market === position.market)[0]
-                if (pnlHistory.pnls.length === this.historyLength) {
-                    const bollingerBands = Bands.getBollingerBands(pnlHistory.pnls, 5)
-                    const lower = bollingerBands.lower[pnlHistory.pnls.length - 1]
-                    const current = pnlHistory.pnls[pnlHistory.pnls.length - 1]
-                    const upper = bollingerBands.upper[pnlHistory.pnls.length - 1]
-                    const advice = this.getAdvice(lower, current, upper, freeCollateralPercentage)
-                    console.log(advice)
+                const bollingerBands = Bands.getBollingerBands(pnlHistory.pnls, 9)
+                const lower = bollingerBands.lower[pnlHistory.pnls.length - 1]
+                const current = pnlHistory.pnls[pnlHistory.pnls.length - 1]
+                const upper = bollingerBands.upper[pnlHistory.pnls.length - 1]
+                const advice = this.getAdvice(lower, current, upper, freeCollatPercentage)
+                console.log(`${advice} ${position.market}`)
+                if (pnlHistory.pnls.length === this.historyLength || advice === "Celebrate") {
                     const wallet = await LocalWallet.fromMnemonic(this.mnemonic, BECH32_PREFIX);
                     const subaccount = new SubaccountClient(wallet, 0);
                     await this.optimizePosition(position, subaccount, advice)
                 }
             }
-        }
-    }
-
-    private getAdvice(lower: number, current: number, upper: number, freeCollateralPercentage: number) {
-        if (current <= lower && freeCollateralPercentage > this.targetCollateralPercentage) {
-            return "Buy"
-        } else if (current >= upper || freeCollateralPercentage < this.minCollateralPercentage) {
-            return "Sell"
-        } else {
-            return "Relax"
         }
     }
 
@@ -97,30 +90,42 @@ export class FreedomCashProvider {
     private async optimizePosition(position: any, subaccount: any, advice: string) {
         const marketData = (await this.indexerClient.markets.getPerpetualMarkets(position.market)).markets[position.market];
         const id = `${this.roundCounter}-${position.market}`
-        let goodTilTimeInSeconds1 = OrderTimeInForce.IOC
-        let side, price
-        if (advice === "Sell" && Math.abs(position.size) > marketData.stepSize) {
-            console.log(`taking profits with ${position.market}`)
-            side = OrderSide.SELL
-            price = (side === OrderSide.BUY) ? marketData.oraclePrice * 1.001 : marketData.oraclePrice * 0.999
-        } else if (advice === "Buy") {
-            console.log(`increasing the exposure for position ${position.market}`)
-            side = OrderSide.BUY
-            price = (side === OrderSide.BUY) ? marketData.oraclePrice * 1.001 : marketData.oraclePrice * 0.999
+        let size = marketData.stepSize
+        let side, price, goodTilTimeInSeconds1
+        if (advice === "Increase") {
+            side = (position.side === "LONG") ? OrderSide.BUY : OrderSide.SELL
+            goodTilTimeInSeconds1 = OrderTimeInForce.IOC
+        } else if (advice === "Decrease" && Math.abs(position.size) > marketData.stepSize) {
+            side = (position.side === "LONG") ? OrderSide.SELL : OrderSide.BUY
             goodTilTimeInSeconds1 = OrderTimeInForce.GTT
+        } else if (advice === "Celebrate" && Math.abs(position.size) > marketData.stepSize) {
+            side = (position.side === "LONG") ? OrderSide.SELL : OrderSide.BUY
+            goodTilTimeInSeconds1 = OrderTimeInForce.GTT
+            size = Math.abs(position.size) - marketData.stepSize
+            console.log(`position.size: ${position.size}`)
+            console.log(`marketData.stepSize: ${marketData.stepSize}`)
+            console.log(`size: ${size}`)
         } else {
             return
         }
-
-        await this.compositeClient.placeOrder(subaccount, position.market, OrderType.MARKET, side, price, marketData.stepSize, id, OrderTimeInForce.GTT, goodTilTimeInSeconds1, OrderExecution.DEFAULT)
-        await this.sleep(9000)
+        price = (side === OrderSide.BUY) ? marketData.oraclePrice * 1.001 : marketData.oraclePrice * 0.999
+        await this.compositeClient.placeOrder(subaccount, position.market, OrderType.MARKET, side, price, size, id, OrderTimeInForce.GTT, goodTilTimeInSeconds1, OrderExecution.DEFAULT)
+        await this.sleep(9)
     }
 
-    private sleep(ms: number) {
-        return new Promise((resolve) => {
-            setTimeout(resolve, ms);
-        });
+    private getAdvice(lower: number, current: number, upper: number, freeCollateralPercentage: number) {
+        if (current <= lower && freeCollateralPercentage > this.targetCollateralPercentage) {
+            return "Increase"
+        } else if (current >= upper || freeCollateralPercentage < this.minCollateralPercentage) {
+            return "Decrease"
+        } else if (current >= this.celebrateAt) {
+            return "Celebrate"
+        } else {
+            return "Relax"
+        }
     }
+
+    private sleep(seconds: number) { return new Promise((resolve) => { setTimeout(resolve, seconds * 1000) }) }
 }
 
 config()
