4709c94d254fd41205eec7b1090d084652b23ed0
Monique Baumann <monique@freedomcash.org>
2024-11-03 01:02:38 +0100

    celebrate

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ historyLength=60
 celebrateAt=1
 intervalLength=24
 targetCollateralPercentage=45
-minCollateralPercentage=25
+minCollateralPercentage=24
 stepSizeFactor=1
-spreadFactor=18
+spreadFactor=24
 
 pm2 start ts-node --name=collect -- -P tsconfig.json collector.ts $historyLength $celebrateAt $intervalLength $targetCollateralPercentage $minCollateralPercentage $stepSizeFactor $spreadFactor
