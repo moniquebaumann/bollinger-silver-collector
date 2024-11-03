2fdef6e30cc1bda3cba7b0c8127ab42c61cd05f4
Monique Baumann <monique@freedomcash.org>
2024-11-03 21:19:58 +0100

    free the world

--- start-locally.sh
+++ start-locally.sh
@@ -3,6 +3,6 @@ celebrateAt=1.61803398874989
 intervalLength=9
 targetCollateralPercentage=45
 minCollateralPercentage=24
-spreadFactor=18
+spreadFactor=3
 
 ts-node collector.ts $historyLength $celebrateAt $intervalLength $targetCollateralPercentage $minCollateralPercentage $spreadFactor 
